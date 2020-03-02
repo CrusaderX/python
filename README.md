59dcd2b6f7b6d5705def7dcd07d27e624ccc5040
CrusaderX <thepliable2@gmail.com>
2020-03-02 22:22:07 +0300

    Add crawler and update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# python
+Git repository with some examples of python code. Almost all code will be connected with boto3 library.
+
+`aws_describe_cloudfront.py` - script to invalidate CloudFront cache for distribution. Written in async manner with `aiobotocore` and `asyncio`.
---
+++ aws_describe_cloudfront.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+
+import time
+import boto3
+import os
+import asyncio
+import argparse
+import aiobotocore
+
+
+parser = argparse.ArgumentParser(description='Invalidate cache for cloudfront distribution')
+parser.add_argument('--tag', help='CloudFront tag',
+                    required=True)
+parser.add_argument('--path', help='Distribution path to invalidate. Default: /*',
+                    required=True)
+args = parser.parse_args()
+
+async def describeDistributions() -> list:
+  """
+    Get all CloudFront distributions and return they ARNs.
+  """
+  distributions = await client.list_distributions()
+  arns = []
+  for distribution in distributions['DistributionList']['Items']:
+    arns.append({'distributionId': distribution['Id'], 'distributionArn': distribution['ARN']})
+  return arns
+
+
+async def describeDistributionsTags(distributionId: str, distributionArn: str) -> dict:
+  """
+    Return distribution tags.
+  """
+  return { 
+    'distributionId': distributionId,
+    'distributionTags': (await client.list_tags_for_resource(Resource=distributionArn))['Tags']['Items']
+  }
+
+async def createInvalidation(distributionId: str, path: str = '/*') -> dict:
+  """
+    Invalidate distribution cache for root path.
+  """
+  return await client.create_invalidation(
+    DistributionId = distributionId,
+    InvalidationBatch = {
+      'Paths': {
+        'Quantity': 1,
+        'Items': [
+          path
+        ],
+      },
+      'CallerReference': datetime.now(tz=None).strftime("%H:%M:%S.%f:%b:%d:%Y")
+    }
+  )
+
+
+async def main():
+  global client
+  
+  session = aiobotocore.get_session()
+  client = session.create_client('cloudfront')
+    
+  tags  = []
+  tasks = []
+  
+  distributions = await describeDistributions()
+  
+  for distribution in distributions:
+    tasks.append(describeDistributionsTags(distribution['distributionId'], distribution['distributionArn']))
+    
+  for task in await asyncio.gather(*tasks):
+    tags.append({ 'distributionId': task['distributionId'], 'distributionTags': task['distributionTags'] })
+
+  for distribution in tags:
+    for tag in distribution['distributionTags']:  
+      if tag['Key'] == 'Name' and tag['Value'] == args.tag:
+        distributionId = distribution['distributionId']
+
+  if distributionId:
+    invalidationResponse = await createInvalidation(distributionId)
+    print(invalidationResponse)
+
+
+asyncio.run(main())
